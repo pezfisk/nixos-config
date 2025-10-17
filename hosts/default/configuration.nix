da8c29c5878b29d4441ba59ee96132b0afbb6ae1
pezfisk <marcorguezhdez@gmail.com>
2025-10-17 09:35:02 +0200

    chore: actually fix flatpak-repo

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -22,6 +22,8 @@
 
   systemd.services.flatpak-repo = {
     wantedBy = [ "multi-user.target" ];
+    after = [ "network-online.target" ];
+    wants = [ "network-online.target" ];
     path = [ pkgs.flatpak ];
     script = ''
     	flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
